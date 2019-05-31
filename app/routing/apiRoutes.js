630c6401f486e31174b4f96b1f9ae88e4aa5f5bb
Steve Long <steve@gridtop.com>
2019-05-31 14:14:25 -0500

    setting up survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,12 @@
 
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+
+app.get("/api/friends", function(req, res) {
+  //display all people in ./app/data/friends.js
+});
+
+app.post("/api/friends", function(req, res) {
+  // ingest survey results, and handle matching logic
+});
