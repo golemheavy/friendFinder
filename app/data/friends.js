519a0ca2a018f35093b0f5feac0e588482b23469
Steve Long <steve@gridtop.com>
2019-06-05 14:35:52 -0500

    calculating a per-dimension distance

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 module.exports = {
 friendsArr : [
 		{
-			"name":"Ahmed",
+			"name":"Data",
 			"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
 			"scores":[
 						5,
@@ -18,6 +18,70 @@ friendsArr : [
 		},
 		{
 			"name":"Lore",
+			"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+			"scores":[
+						5,
+						1,
+						4,
+						4,
+						5,
+						1,
+						2,
+						5,
+						4,
+						1
+			]
+		},
+		{
+			"name":"Peppermint Patty",
+			"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+			"scores":[
+						5,
+						1,
+						4,
+						4,
+						5,
+						1,
+						2,
+						5,
+						4,
+						1
+			]
+		},
+		{
+			"name":"Knight Rider",
+			"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+			"scores":[
+						5,
+						1,
+						4,
+						4,
+						5,
+						1,
+						2,
+						5,
+						4,
+						1
+			]
+		},
+		{
+			"name":"Tomb Raider",
+			"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+			"scores":[
+						5,
+						1,
+						4,
+						4,
+						5,
+						1,
+						2,
+						5,
+						4,
+						1
+			]
+		},
+		{
+			"name":"Spock",
 			"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
 			"scores":[
 						5,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,14 @@ var friendData = require("../data/friends.js");
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 module.exports = function(app) {
+	
+	function rankMatches(scoresArr) {
+		
+		return friendData.friendsArr.map(x => {
+			return x.scores.map((y, index) => {return Math.abs(y - scoresArr[index]);});
+		});
+	}
+	
 	app.get("/api/friends", function(req, res) {
 		//res.sendFile(path.join(__dirname, '../data/friends.js'));
 		res.json(friendData);
@@ -14,8 +22,12 @@ module.exports = function(app) {
 	
 	app.post("/api/friends", function(req, res) {
 		// ingest survey results, and handle matching logic
-		friendData.friendsArr.push(req.body);
-		res.json(true);
+		// push req.body into object and rewrite whole file
+		// friendData.friendsArr.push(req.body); this works but is not needed because we are not going to add user to friends data object
+		//res.json(true);
+		//console.log(req.body.scores);
+		console.log(rankMatches(req.body.scores));
+		//rankMatches(req.body.scores);
 		
 	});
 };
