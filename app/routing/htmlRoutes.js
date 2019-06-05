c8360fb25efc953172542d0afa10a792be5a9b26
Steve Long <steve@gridtop.com>
2019-06-05 12:36:55 -0500

    serving files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,24 @@
+var path = require("path");
+
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+
+
+module.exports = function(app) {
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, '../data/friends.js'));
+	});
+	
+	app.get("/", function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+	
+	app.get("/home", function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+	
+	app.get('*',function (req, res) {
+        res.redirect('/');
+	});
+	
+};
