c817b9ac0310647019893fee26a025364ca60a45
Steve Long <steve@gridtop.com>
2019-06-05 14:04:01 -0500

    it is updating the friends data object, but not writing it out to file yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,6 @@
 var path = require("path");
+
+var friendData = require("../data/friends.js");
 //should contain two routes:
 
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
@@ -6,10 +8,14 @@ var path = require("path");
 
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
-		res.sendFile(path.join(__dirname, '../data/friends.js'));
+		//res.sendFile(path.join(__dirname, '../data/friends.js'));
+		res.json(friendData);
 	});
 	
 	app.post("/api/friends", function(req, res) {
 		// ingest survey results, and handle matching logic
+		friendData.friendsArr.push(req.body);
+		res.json(true);
+		
 	});
 };
