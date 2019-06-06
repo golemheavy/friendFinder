c81058acc541ea37d49a453816b22971f2662ac8
Steve Long <steve@gridtop.com>
2019-06-06 00:27:36 -0500

    cleaned up some stuff in htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,5 @@
 var path = require("path");
 
-// A GET Route to /survey which should display the survey page.
-// A default, catch-all route that leads to home.html which displays the home page.
-
-
 module.exports = function(app) {
 	app.get("/survey", function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
